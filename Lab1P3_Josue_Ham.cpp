1a3e91e9b94ad22af02cbc5d1f7b232f16fffdee
SKX Kafei <josue.ham@unitec.edu>
2023-04-21 15:20:52 -0600

    Ejercicio 1 casi terminado

--- Lab1P3_Josue_Ham.cpp
+++ Lab1P3_Josue_Ham.cpp
@@ -16,8 +16,42 @@ int main()
 		cin >> opcion;
 		switch (opcion) {
 		case 1: {
-			cout << "Error Numerico";
+			cout << "Error Numerico" << endl;
+			cout << "Ingrese el valor de N: ";
+			int N;
+			cin >> N;
+			float X1 = 1/(float)N;
+			double X2 = 1/(double)N;
+			long double X3 = 1/(long double)N;
+
+			float S1 =0;
+			double S2 =0;
+			long double S3 =0;
+
 			
+
+			for (int i = 0; i < N; i++){
+				S1+= X1;
+				S2+= X2;
+				S3+= X3;
+			}
+
+			float E1 = 1.0-S1;
+			double E2 = 1.0-S2;
+			long double E3 = 1.0-S3;
+
+			if (E1 <= 0){
+				E1*-1;
+			}
+			else if (E2 < 0){
+				E2*-1;
+			}
+			else if (E3 < 0) {
+				E3*-1;
+			}
+			cout << "Usando Float: " << endl << "E: " << E1 << endl;
+			cout << "Usando Double: " << endl << "E: " << E2 << endl;
+			cout << "Usando Long Double: " << endl << "E: " << E3 << endl;
 		}
 		break;
 
@@ -34,12 +68,13 @@ int main()
 			cin >> B;
 
 			int M = 0;
-			if (P > 0 && B!=0){
-				for (int i = 0; i < P; i++){
-					M+= A/B;
+			for (int i = 0; i < P; i++) {
+				if (P > 0 && B != 0) {
+					M += A / B;
 					B--;
-				}
+				}		
 			}
+			
 
 			cout << "Valor de M: " << M << endl;
 		}
